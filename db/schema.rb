2bf99598c0c713eca8c9905d3af41bd2b3e83a5e
mohira <m.ohira@nowall.co.jp>
2017-01-25 19:05:16 +0900

    Bookモデル生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125100347) do
+ActiveRecord::Schema.define(version: 20170125100505) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
