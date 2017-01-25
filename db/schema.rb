5e9270f6689456a2b32efea449ced9d0df1fce97
mohira <m.ohira@nowall.co.jp>
2017-01-25 19:12:07 +0900

    Reviewモデルの準備

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125100505) do
+ActiveRecord::Schema.define(version: 20170125100610) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
     t.integer  "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "book_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id", "user_id"], name: "index_reviews_on_book_id_and_user_id", unique: true
+    t.index ["book_id"], name: "index_reviews_on_book_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
