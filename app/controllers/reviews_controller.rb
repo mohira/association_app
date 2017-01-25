e0674bc81ac262ee44c0e16fec5a5ff2669927df
mohira <m.ohira@nowall.co.jp>
2017-01-25 20:22:44 +0900

    新規レビュー保存処理

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,31 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def new
     @book = Book.find(params[:book_id])
     @review = Review.new
   end
+
+  def create
+    # これがないとバリデーションエラー時に@bookの情報を失う
+    # つまりbook.titleなどがとれなくなる
+    @book = Book.find(params[:book_id])
+
+    @review = Review.new(
+      body: review_params[:body],
+      user_id: current_user.id,
+      book_id: params[:book_id]
+    )
+
+    if @review.save
+      redirect_to book_path(@review.book)
+    else
+      render :new
+    end
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:body, :user_id, :book_id)
+    end
 end
